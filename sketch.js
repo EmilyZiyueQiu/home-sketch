03ee18ccf4aa0737a9376ed798f1b49ec10f109e
EmilyZiyueQiu <61444207+EmilyZiyueQiu@users.noreply.github.com>
2020-03-02 16:34:37 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,60 @@
+//variable for drawing size
+let x = 800;
+let y = 500;
+//variable for bouncing ball
+let s = 255;
+let maxHeight = 480;
+//variable for shadow
+let maxWidth =100;
+//variable for flower
+let o =0;
+//variable for cushion color changing
+let tr = 0;
+let tg =0;
+let tb =0;
 
+
+// function setup and draw----------------------------------------
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-    background(226,255,219);	
-    	
-//***size of the whole drawing	
-	let x = 800;
-    let y = 500;	
+	background("black");
+	frameRate(20);		
+}
 
-//***all stuffs except two cushions
+
+function draw() {	
+//***a tool: to find positions
+    console.log(mouseX,mouseY);	
+	
+	flower(140,170);
+	flower(1200,650);
+
+
+	//***all stuffs except two cushions
 	blueFloor(x,y);
-    pinkCurtain(x,y);		
-    holders();	
+	pinkCurtain(x,y);		
+   	holders();	
     tableSurface(x,y);
     pencils();		
     pencilVase();	
     ornament();	
     eraser();	
-    easel();	
-    drawingBoard();	
-    orangeBalloon();	
-    signature();	
-}
-
-function draw() {	
-//***a tool: to find positions
-    console.log(mouseX,mouseY);	
-    
+    easel();		
+	signature();
+	
 //***two cushions
 	gridCushion();
-    triangleCushion();
+	triangleCushion();
+	drawingBoard();
+	orangeBalloon();
+	popHeart(760,280,50);
+	heart(120,350,80);
+	heart(170,550,60);
+	heart(120,700,40);
+	heart(1300,450,80);
+	heart(1200,350,60);
+	heart(1350,200,70);
+	heart(1200,100,55);
 }
 
 
@@ -111,14 +137,18 @@ function drawingBoard(){
 }
 
 function orangeBalloon(){
-    noStroke();
+
+	noStroke();
 	fill(0,0,0,50);
-	ellipse(897,570,100,30);//shadow
+	let shadowWidth = maxWidth - 40 * (Math.cos(frameCount/10));
+	ellipse(897,570,shadowWidth,30);//shadow
 	fill(255,138,34);
-	ellipse(880,520,120,120);//the ball
+	let bally = maxHeight - 40 * (Math.cos(frameCount/10));
+	ellipse(880,bally,120,120);//the ball
 }
 
 function signature(){
+	noStroke();
     fill(102,51,0);
 	textSize(30);
 	textStyle(BOLD);
@@ -134,19 +164,19 @@ function gridCushion(){
 			stroke(236,255,245);
 			strokeWeight(0.5);
 			if(x+y==0||x*y==4){
-			fill(0,51,51);
+			fill("white");
 			rect(x*40+10,y*40+10,40,40);
 			}
 			else if(x+y==2||x*y==1){
-			fill(133,141,147);
+			fill("white");
 			rect(x*40+10,y*40+10,40,40);	
 			}
 			else if(x+y==3){
-			fill(250,255,247);
+			fill(random(255),random(255),random(50,255),100);
 			rect(x*40+10,y*40+10,40,40);			
 			}
 			else{
-			fill(195,201,189);
+				fill(random(255),random(255),random(50,255),100);
 			rect(x*40+10,y*40+10,40,40);
 			}
 		}
@@ -159,7 +189,40 @@ function triangleCushion(){
 	translate(600, 450);
 	rotate(PI / 2.1);
 	noStroke();
-	fill(42,50,69);
+
+	if (mouseX<200){
+		tr = 0;
+		tg =0;
+		tb =102;
+	}else if (mouseX>200&&mouseX<400){
+		tr =76;
+		tg =0;
+		tb =153;
+	}else if(mouseX<600&&mouseX>400){
+		tr =153;
+		tg =76;
+		tb =0;
+	}else if(mouseX<800&&mouseX>600){
+		tr =0;
+		tg =102;
+		tb =51;
+	}else if(mouseX<1000&&mouseX>800){
+		tr =153;
+		tg =0;
+		tb =76;
+	}else if(mouseX<1200&&mouseX>1000){
+		tr =160;
+		tg =160;
+		tb =160;
+	}else {
+		tr =146;
+		tg =17;
+		tb =17;
+	}
+
+
+	fill(tr,tg,tb);
+
 	rect(-42,-40,165,165);
 	pop();
 	
@@ -174,3 +237,131 @@ function triangleCushion(){
 	triangle(546,536,533,550,560,542);
 	triangle(622,546,635,546,625,558);
 }
+
+function flower(f,l){
+
+	//let o =50 - 50 * abs((Math.cos(frameCount/20)));
+	
+	if(o<40){
+	noStroke();
+	fill("white");
+	ellipse(f,l-2.1*o,o,3*o);//top
+
+	push();//right-top
+	translate(f+1.5*o,l-1.5*o);
+	rotate(PI/4);
+	ellipse(0,0,o,3*o);
+	pop();
+
+	ellipse(f+2.1*o,l,3*o,o);//right
+
+	push();//right-bottom
+	translate(f+1.5*o,l+1.5*o);
+	rotate(PI/4);
+	ellipse(0,0,3*o,o);
+	pop();
+
+	ellipse(f,l+2.1*o,o,3*o);//bottom
+
+	push();//left-bottom
+	translate(f-1.5*o,l+1.5*o);
+	rotate(3*PI/4);
+	ellipse(0,0,3*o,o);
+	pop();
+
+	ellipse(f-2.1*o,l,3*o,o);//left
+
+	push();//left-top
+	translate(f-1.5*o,l-1.5*o);
+	rotate(3*PI/4);
+	ellipse(0,0,o,3*o);
+	pop();
+
+	o = o+0.1;
+} else{
+	noStroke();
+	fill("black");
+	ellipse(f,l-2.1*o,o,3*o);//top
+
+	push();//right-top
+	translate(f+1.5*o,l-1.5*o);
+	rotate(PI/4);
+	ellipse(0,0,o,3*o);
+	pop();
+
+	ellipse(f+2.1*o,l,3*o,o);//right
+
+	push();//right-bottom
+	translate(f+1.5*o,l+1.5*o);
+	rotate(PI/4);
+	ellipse(0,0,3*o,o);
+	pop();
+
+	ellipse(f,l+2.1*o,o,3*o);//bottom
+
+	push();//left-bottom
+	translate(f-1.5*o,l+1.5*o);
+	rotate(3*PI/4);
+	ellipse(0,0,3*o,o);
+	pop();
+
+	ellipse(f-2.1*o,l,3*o,o);//left
+
+	push();//left-top
+	translate(f-1.5*o,l-1.5*o);
+	rotate(3*PI/4);
+	ellipse(0,0,o,3*o);
+	pop();
+
+	//let o returns value 0
+	o = 0;
+}
+	fill("#FFE326");
+	ellipse(f,l,50,50);
+}
+
+function popHeart(he,ha){
+	let hr = map(mouseX,0,windowWidth,10,70);
+	stroke("white");
+	strokeWeight(4);
+	fill(243,0,0,200);
+	beginShape();
+	vertex(he,ha);
+	bezierVertex(he-hr/2,ha-hr/2,he-hr,ha+hr/3,he,ha+hr);
+	bezierVertex(he+hr,ha+hr/3,he+hr/2,ha-hr/2,he,ha);
+	endShape();
+}
+
+function heart(he,ha,hr){
+	stroke("white");
+	strokeWeight(7);
+	noFill();
+	beginShape();
+	vertex(he,ha);
+	bezierVertex(he-hr/2,ha-hr/2,he-hr,ha+hr/3,he,ha+hr);
+	bezierVertex(he+hr,ha+hr/3,he+hr/2,ha-hr/2,he,ha);
+	endShape();
+}
+
+
+
+// failed try, later revised
+
+/*
+function mouseDragged(){
+		stroke(s);
+		strokeWeight(7);
+		line(mouseX,mouseY,pmouseX,pmouseY);
+	}
+	
+	
+	function mousePressed(){
+		s=color (random(100,255), random(0,255),random(0,255));
+	}
+	
+	function keyPressed(){
+		if(key===" "){
+			background(random(255), random(255),random(255));
+		}
+	}
+*/
